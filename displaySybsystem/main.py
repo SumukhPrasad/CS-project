1dc534d7bccf6cc2ee98f9abb0df65bd67c85710
Sumukh Prasad <sumukhprasad@icloud.com>
2023-09-29 19:28:25 +0530

    Moved to pygame, added grids and axes.

--- displaySybsystem/main.py
+++ displaySybsystem/main.py
@@ -1,36 +1,34 @@
-import glfw
+import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import time
 
 
-if not glfw.init():
-    raise RuntimeError("Failed to initialize GLFW. Check if GLFW has been correctly installed. Exiting.")
+if not pygame.init():
+    raise RuntimeError("Failed to initialize PyGame. Check if PyGame has been correctly installed. Exiting.")
 
 class DisplaySubsystem:
     def __init__(self, w, h, title, runtimeFunc):
-        self.window = glfw.create_window(w, h, title, None, None)
-        if not self.window:
-            glfw.terminate()
-            raise RuntimeError("Failed to create GLFW window. Exiting.")
-        
-        glfw.window_hint(glfw.SAMPLES, 4)
-        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
-        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-        glfw.make_context_current(self.window)
+        run = True
+        display = (w, h)
+        pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
+
         glViewport(0, 0, w, h)
         
         gluPerspective(45, 1, 0.1, 50.0)
         glTranslatef(0.0,0.0, -5)
         self.tick = 1
         self._starttime = time.time()
-        while not glfw.window_should_close(self.window):
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            
             try:
                 runtimeFunc()
                 self.print_debug()
+                self.print_grids()
                 self.tick+=1
             except GLError as glex:
                 print(f"GLError{glex.err}: {glex}")
@@ -38,8 +36,7 @@ class DisplaySubsystem:
                 print("An error has occurred.", e)
                 break
 
-            glfw.poll_events()
-            glfw.swap_buffers(self.window)
+            pygame.display.flip()
 
     def glut_print( self, x,  y,  text, r,  g , b , a):
         glColor3fv((r, g, b))
@@ -48,6 +45,72 @@ class DisplaySubsystem:
         for ch in text :
             glutBitmapCharacter( GLUT_BITMAP_9_BY_15, ctypes.c_int( ord(ch) ) )
         glPopMatrix();
+    
+    def print_grids(self):
+        glLineWidth(1.0)  # Reset line width to default
+        grid_spacing = 1.0  # Adjust this value to control grid density
+        grid_size = 10.0  # Adjust this value to control grid size
+
+        glColor3f(0.5, 0.5, 0.5)  # Gray color for the grid lines
+
+        # Draw grids in the XY plane
+        glBegin(GL_LINES)
+        for i in range(int(-grid_size / grid_spacing), int(grid_size / grid_spacing) + 1):
+            x = i * grid_spacing
+            glVertex3f(x, -grid_size, 0.0)
+            glVertex3f(x, grid_size, 0.0)
+
+            y = i * grid_spacing
+            glVertex3f(-grid_size, y, 0.0)
+            glVertex3f(grid_size, y, 0.0)
+        glEnd()
+
+        # Draw grids in the XZ plane
+        '''glBegin(GL_LINES)
+        for i in range(int(-grid_size / grid_spacing), int(grid_size / grid_spacing) + 1):
+            x = i * grid_spacing
+            glVertex3f(x, 0.0, -grid_size)
+            glVertex3f(x, 0.0, grid_size)
+
+            z = i * grid_spacing
+            glVertex3f(-grid_size, 0.0, z)
+            glVertex3f(grid_size, 0.0, z)
+        glEnd()
+
+        # Draw grids in the YZ plane
+        glBegin(GL_LINES)
+        for i in range(int(-grid_size / grid_spacing), int(grid_size / grid_spacing) + 1):
+            y = i * grid_spacing
+            glVertex3f(0.0, y, -grid_size)
+            glVertex3f(0.0, y, grid_size)
+
+            z = i * grid_spacing
+            glVertex3f(0.0, -grid_size, z)
+            glVertex3f(0.0, grid_size, z)
+        glEnd()'''
+
+        glLineWidth(5.0)  # Set line width for the axes
+
+        # X-axis (Red)
+        glColor3f(1.0, 0.0, 0.0)  # Red color
+        glBegin(GL_LINES)
+        glVertex3f(grid_size, 0.0, 0.0)
+        glVertex3f(-grid_size, 0.0, 0.0)
+        glEnd()
+
+        # Y-axis (Green)
+        glColor3f(0.0, 1.0, 0.0)  # Green color
+        glBegin(GL_LINES)
+        glVertex3f(0.0, grid_size, 0.0)
+        glVertex3f(0.0, -grid_size, 0.0)
+        glEnd()
+
+        # Z-axis (Blue)
+        glColor3f(0.0, 0.0, 1.0)  # Blue color
+        glBegin(GL_LINES)
+        glVertex3f(0.0, 0.0, grid_size)
+        glVertex3f(0.0, 0.0, -grid_size)
+        glEnd()
 
     def print_debug(self):
         self.glut_print(10, 90, f"-- DEBUG --", 0.0, 0.0, 0.0, 0.0)
--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 from displaySybsystem.main import *
 from displaySybsystem.dsHelpers import *
-from pywavefront import Wavefront
-
-scene = Wavefront('models/toy_car.obj')
-scene_box = (scene.vertices[0], scene.vertices[0])
-for vertex in scene.vertices:
-    min_v = [min(scene_box[0][i], vertex[i]) for i in range(3)]
-    max_v = [max(scene_box[1][i], vertex[i]) for i in range(3)]
-    scene_box = (min_v, max_v)
-scene_trans    = [-(scene_box[1][i]+scene_box[0][i])/2 for i in range(3)]
-
-scaled_size    = 5
-scene_size     = [scene_box[1][i]-scene_box[0][i] for i in range(3)]
-max_scene_size = max(scene_size)
-scene_scale    = [scaled_size/max_scene_size for i in range(3)]
-
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -22,23 +7,31 @@ from OpenGL.GLU import *
 def runner():
      glClearColor(0.7, 0.75, 0.75, 1)
      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-     glMatrixMode(GL_PROJECTION)
-     glLoadIdentity()
-     glMatrixMode(GL_MODELVIEW)
-     glLoadIdentity()
+     glMatrixMode (GL_MODELVIEW)
+
+     light = (1,1,1)
+     faces = [
+          [(1,0,0), (0,1,0), (0,0,1)],
+          [(1,0,0), (0,0,-1), (0,1,0)],
+          [(1,0,0), (0,0,1), (0,-1,0)],
+          [(1,0,0), (0,-1,0), (0,0,-1)],
+          [(-1,0,0), (0,0,1), (0,1,0)],
+          [(-1,0,0), (0,1,0), (0,0,-1)],
+          [(-1,0,0), (0,-1,0), (0,0,1)],
+          [(-1,0,0), (0,0,-1), (0,-1,0)],
+     ]
+     glEnable(GL_CULL_FACE)
      glEnable(GL_DEPTH_TEST)
-
+     glCullFace(GL_BACK)
+     glRotatef(1,1,1,1)
      glBegin(GL_TRIANGLES)
-     glPushMatrix()
 
-     for mesh in scene.mesh_list:
-          glBegin(GL_TRIANGLES)
-          for face in mesh.faces:
-               for vertex_i in face:
-                    glVertex3f(*scene.vertices[vertex_i])
-          glEnd()
+     for face in faces:
+         color = shade(face,light)
+         for vertex in face:
+             glColor3fv((color[0], color[1], color[2]))
+             glVertex3fv(vertex)
 
-     glPopMatrix()
      glEnd()
 
 ds = DisplaySubsystem(512, 512, "inferno", runner)
