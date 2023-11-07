fa8eaa7cd7a51fec6ba608057a3b7892074c7cf6
Sumukh Prasad <sumukhprasad@icloud.com>
2023-11-07 21:45:46 +0530

    Added db to game board.

--- gameBoardSubsystem/main.py
+++ gameBoardSubsystem/main.py
@@ -1,6 +1,8 @@
 from dbSubsystem.main import dbSubsystem
 
 class gameBoard:
-     def __init__(self, name): ...
+     def __init__(self, name):
+          self.name = name
+          self.dbsub = dbSubsystem()
 
      def save(self): ...
